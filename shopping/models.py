e18bacfca3a0dc6dc2fb6923e3cde3deed5ad5c9
maortizc2 <maortizc2@eafit.edu.co>
2025-02-18 06:39:20 -0500

    actualizacion de superusuario

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productDescription = models.CharField(max_length=500)
+    productPicture = models.ImageField(upload_to='productImages/')
