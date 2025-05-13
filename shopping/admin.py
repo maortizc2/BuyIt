a0ac9a6c7bc927031c62b26603d8e437a54aed96
Jose Alejandro Jimenez <jajimenez4@eafit.edu.co>
2025-05-12 23:19:42 -0500

    💡 Comparador de productos funcional + UI animada

--- shopping/admin.py
+++ shopping/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 from .models import Product, SearchHistory
 
-<<<<<<< HEAD
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(SearchHistory)
-=======
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('productName', 'productBrand', 'productPrice', 'productCategory', 'productStock', 'created_at')
     search_fields = ('productName', 'productBrand', 'productCategory')
     list_filter = ('productCategory', 'productBrand', 'productOrigin')
     ordering = ('-created_at',)
->>>>>>> cc08e4e (💡 Comparador de productos funcional + UI animada)
