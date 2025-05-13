e62ddba762c455e614df1363635dd7f5014e895e
vale-agui <vaguilarc@eafit.edu.co>
2025-05-12 21:57:24 -0500

    se agrega la busqueda y los productos favoritos

--- shopping/views.py
+++ shopping/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, SearchHistory
 from django.db.models import Q
 from .comparador import comparar_dos_productos_ai
 
@@ -32,16 +32,24 @@ def favoritos_view(request):
 def search_products(request):
     query = request.GET.get('q')
     results = Product.objects.none()
+
     if query:
         results = Product.objects.filter(
             Q(productName__icontains=query) | Q(productDescription__icontains=query)
         )
+        if request.user.is_authenticated:
+            SearchHistory.objects.create(user=request.user, query=query)
+
     context = {
         'query': query,
         'results': results,
     }
     return render(request, 'buscar.html', context)
 
+@login_required
+def historial_busquedas(request):
+    historial = request.user.search_history.order_by('-timestamp')[:20]  # últimas 20
+    return render(request, 'historial.html', {'historial': historial})
 
 def productos_externos(request):
     productos = Product.objects.all().order_by('-created_at')
